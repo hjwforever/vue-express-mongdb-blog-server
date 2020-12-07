051ec125911f74933e278ae600f5b4e8d137b484
hjwforever <1326153330@qq.com>
2020-12-07 16:20:54 +0800

    update

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ require("./routes/post.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
